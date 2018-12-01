9ea431dea72d85b2ccd94696283c12115687208a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2018-12-01 21:02:25 +0300

    Cleanups

--- src/01.kt
+++ src/01.kt
@@ -1,21 +1,18 @@
 fun main() {
     val fin = java.io.File("data/01.txt").inputStream()
-    val text = fin.bufferedReader().readText()
-    fin.close()
-    val deltas = text.trimEnd().lines().map { line -> line.toInt() }
+    val deltas = fin.bufferedReader().useLines {
+        it.map { line -> line.toInt() }.toList()
+    }
     println("part 1: ${deltas.sum()}")
     var s = 0
-    var seen = mutableSetOf<Int>()
-    var cnt = 0
+    val seen = mutableSetOf<Int>()
     outer@ while (true) {
         for (delta in deltas) {
             s += delta
-            cnt++
-            if (seen.contains(s)) {
+            if (!seen.add(s)) {
                 println("part 2: $s")
                 break@outer
             }
-            seen.add(s)
         }
     }
 }
