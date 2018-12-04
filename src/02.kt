e4a0c041e6645d755e50a2da0704f0335d9915ff
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2018-12-04 19:42:20 +0300

    More idiomatic, perhaps

--- src/02.kt
+++ src/02.kt
@@ -1,18 +1,10 @@
-fun <T> freqs(xs: Iterable<T>): Map<T, Int> {
-    val res = mutableMapOf<T, Int>()
-    for (x in xs) {
-        res[x] = (res[x] ?: 0) + 1
-    }
-    return res
-}
-
 fun main() {
     val fin = java.io.File("data/02.txt").inputStream()
     val lines = fin.bufferedReader().useLines { it.toList() }
     var numTwos = 0
     var numThrees = 0
     for (line in lines) {
-        val f = freqs(line.asIterable())
+        val f = line.groupingBy{it}.eachCount()
         if (f.values.contains(2)) {
             numTwos += 1
         }
@@ -22,16 +14,13 @@ fun main() {
     }
     println("part 1: ${numTwos * numThrees}")
 
-    val frags = mutableMapOf<Pair<Int, String>, Int>()
-    for (line in lines) {
-        for (i in 0 until line.length) {
-            val key = Pair(i, line.slice(0 until i) + line.slice(i + 1 until line.length))
-            frags[key] = 1 + (frags[key] ?: 0)
+    lines.map {line ->
+        (0 until line.length).map { i ->
+            Pair(i, line.slice(0 until i) + line.slice(i + 1 until line.length))
         }
-    }
-    for (e in frags) {
-        if (e.value == 2) {
-            println("part 2: ${e.key.second}")
+    }.flatten().groupingBy{it}.eachCount().forEach { p, cnt ->
+        if (cnt == 2) {
+            println("part 2: ${p.second}")
         }
     }
 }
