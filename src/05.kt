37a5259fdf39aaa09dbb72273049bfa7fd208a13
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2018-12-06 22:12:55 +0300

    Day 05, part 2

--- src/05.kt
+++ src/05.kt
@@ -2,15 +2,21 @@ fun opposite(a: Char, b: Char): Boolean {
     return a.toUpperCase() == b.toUpperCase() && a != b
 }
 
-fun main() {
-    val (poly) = java.io.File("data/05.txt").inputStream().bufferedReader().useLines { it.toList() }
+fun collapse(xs: String): Int {
     val res = mutableListOf<Char>()
-    for (c in poly) {
+    for (c in xs) {
         if (!res.isEmpty() && opposite(c, res[res.size - 1])) {
             res.removeAt(res.size - 1)
         } else {
             res.add(c)
         }
     }
-    println("part 1: ${res.size}")
+    return res.size
+}
+
+fun main() {
+    val (poly) = java.io.File("data/05.txt").inputStream().bufferedReader().useLines { it.toList() }
+    println("part 1: ${collapse(poly)}")
+    val allTypes = poly.map { it.toUpperCase() }.toSet()
+    println("part 2: ${allTypes.map { c -> collapse(poly.filter { it.toUpperCase() != c }) }.min()}")
 }
