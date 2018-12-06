e155af2609981986224c7fb50f60026e99d9c5ac
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2018-12-06 17:47:57 +0300

    Day 03, part 2

--- src/03.kt
+++ src/03.kt
@@ -2,15 +2,28 @@ fun main() {
     val lines = java.io.File("data/03.txt").inputStream().bufferedReader().useLines{it.toList()}
     val re = kotlin.text.Regex("""#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""")
 
-    val overlapCnt = lines.asSequence().flatMap{ line ->
+    val idsByPt = lines.asSequence().flatMap { line ->
         val m = re.matchEntire(line)!!
+        val id = m.groups[1]!!.value.toInt()
         val left = m.groups[2]!!.value.toInt()
         val top = m.groups[3]!!.value.toInt()
         val width = m.groups[4]!!.value.toInt()
         val height = m.groups[5]!!.value.toInt()
         (left until left + width).asSequence().flatMap { x ->
-            (top until top + height).asSequence().map { y -> Pair(x, y) }
+            (top until top + height).asSequence().map { y -> Pair(Pair(x, y), id) }
         }
-    }.groupingBy { it }.eachCount().filter { it.value > 1 }.count()
-    println("part1: $overlapCnt")
+    }.groupBy { it.first }.mapValues { it.value.map { it.second } }
+
+    println("part 1: ${idsByPt.filter { it.value.size > 1 }.count()}")
+
+    val overlappingIds = idsByPt.asSequence().flatMap { kv ->
+        val s = kv.value.toSet()
+        if (s.size > 1) {
+            s
+        } else {
+            emptySet()
+        }.asSequence()
+    }.toSet()
+    val allIds = idsByPt.asSequence().flatMap { it.value.asSequence() }.toSet()
+    println("part 2: ${allIds - overlappingIds}")
 }
