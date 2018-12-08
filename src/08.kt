de57667db4de417417830b78c22fe7ed2a0b1c2c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2018-12-08 15:40:51 +0300

    Day 08, part 2

--- src/08.kt
+++ src/08.kt
@@ -1,6 +1,6 @@
 sealed class Task {
     object Child : Task()
-    class Metadata(val count: Int): Task()
+    class Metadata(val numChildren: Int, val count: Int): Task()
 }
 
 fun main() {
@@ -11,22 +11,36 @@ fun main() {
 
     val i = xs.iterator()
     val tasks = mutableListOf<Task>(Task.Child)
+    val nodeValues = mutableListOf<Int>()
     while (!tasks.isEmpty()) {
         val task = tasks.removeAt(tasks.size - 1)
         when (task) {
             is Task.Child -> {
                 val numChildren = i.next()
                 val numMetadata = i.next()
-                tasks.add(Task.Metadata(numMetadata))
-                (1..numChildren).forEach {
-                    tasks.add(Task.Child)
-                }
+                tasks.add(Task.Metadata(numChildren, numMetadata))
+                (1..numChildren).forEach { tasks.add(Task.Child) }
             }
             is Task.Metadata -> {
-                (1..task.count).forEach { metadataSum += i.next() }
+                val children = nodeValues.slice(nodeValues.size - task.numChildren until nodeValues.size)
+                (1..task.numChildren).forEach { nodeValues.removeAt(nodeValues.size - 1) }
+                var nodeValue = 0
+                (1..task.count).forEach {
+                    val m = i.next()
+                    metadataSum += m
+                    if (children.isEmpty()) {
+                        nodeValue += m
+                    } else {
+                        if (m >= 1 && m <= children.size) {
+                            nodeValue += children[m - 1]
+                        }
+                    }
+                }
+                nodeValues.add(nodeValue)
             }
         }
     }
     assert(!i.hasNext())
     println("part 1: $metadataSum")
+    println("part 2: ${nodeValues.single()}")
 }
