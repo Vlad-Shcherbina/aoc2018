1d1635a8d300627685427f1ecd7cc5babcb8482f
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2018-12-09 16:15:21 +0300

    Day 09, part 2

--- src/09.kt
+++ src/09.kt
@@ -1,13 +1,10 @@
-const val numPlayers = 452
-const val lastMarble = 71250
-
 data class Node(val value: Int, var cw: Node?, var ccw: Node?)
 
-fun main() {
+fun solve(numPlayers: Int, lastMarble: Int): Long {
     var cur = Node(0, null, null)
     cur.cw = cur
     cur.ccw =cur
-    val scores = (1..numPlayers).map { 0 }.toMutableList()
+    val scores = (1..numPlayers).map { 0L }.toMutableList()
     for (marble in 1..lastMarble) {
         if (marble % 23 != 0) {
             cur = cur.cw!!
@@ -16,13 +13,18 @@ fun main() {
             new.ccw!!.cw = new
             cur = new
         } else {
-            scores[marble % numPlayers] += marble
+            scores[marble % numPlayers] += marble.toLong()
             for (i in 1..7) cur = cur.ccw!!
-            scores[marble % numPlayers] += cur.value
+            scores[marble % numPlayers] += cur.value.toLong()
             cur.ccw!!.cw = cur.cw
             cur.cw!!.ccw = cur.ccw
             cur = cur.cw!!
         }
     }
-    println("part 1: ${scores.max()}")
+    return scores.max()!!
+}
+
+fun main() {
+    println("part 1: ${solve(452, 71250)}")
+    println("part 2: ${solve(452, 7125000)}")
 }
